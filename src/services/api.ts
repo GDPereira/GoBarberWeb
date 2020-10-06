10e09e1f1a4bf969a94f4c29e5ff48580c204d36
GDPereira <gdelucapereira@gmail.com>
2020-10-06 13:19:38 -0300

    Adjusts env files, api endpoint and react port

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const api = axios.create({
-  baseURL: 'http://localhost:3333',
-});
+const baseURL = `${process.env.REACT_APP_API_URL}:${process.env.REACT_APP_API_PORT}`;
+
+const api = axios.create({ baseURL });
 
 export default api;
